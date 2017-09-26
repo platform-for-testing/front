add572b9ecae05886dfc7f098fc9608782ff8b9b
Yurii Holiuk <yuriiholiuk@gmail.com>
2017-09-26 21:52:36 +0300

    change

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
-  });
+});
 
 app.listen(process.env.PORT || 8080);
