57e4e314bf2530239a8e5244fb2ee60b1a3dab51
tatsiak <tatsiak@gmail.com>
2017-09-28 22:50:20 +0300

    developed adding tests from config started to build second part of test item

--- src/app/all-tests/wraper/test-item/test-item.component.ts
+++ src/app/all-tests/wraper/test-item/test-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Test} from "./test";
 
 @Component({
   selector: 'pt-test-item',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestItemComponent implements OnInit {
   testImagePath: string;
+
+
+  tests: Test[] = [
+      new Test('Тест по Git. Начальный уровень', 4, 10),
+      new Test('Тест по HTML. Средний уровень', 5, 12),
+      new Test('Тест по JS на тему: "Основы. Часть 05', 6, 8),
+  ];
 
   constructor() {
     this.testImagePath = '/assets/images/image.jpeg'
