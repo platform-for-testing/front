31c3d5715c4a2d6f74b4bc6bf93ea388d5568a6e
Sergey Tatsiak <tatsiak@zeoalliance.com>
2017-10-12 17:39:40 +0300

    fixed layouts

--- src/app/pages/all-activations/activations-list-wrapper/activations-list-wrapper.component.ts
+++ src/app/pages/all-activations/activations-list-wrapper/activations-list-wrapper.component.ts
@@ -10,8 +10,8 @@ import {Activation} from "../../models/activation";
 export class ActivationsListWrapperComponent implements OnInit {
 
   activations: Activation[] = [
-      new Activation('activation name', 2, 1, 3, '/assets/images/image.jpeg'),
-      new Activation('another name', 4, 5, 6, '/assets/images/image.jpeg'),
+      new Activation('ACTIVATION NAME', 2, 1, 3, '/assets/images/image.jpeg', true),
+      new Activation('ANOTHER ACTIVATOIN', 4, 5, 6, '/assets/images/image.jpeg', false),
   ];
 
   constructor() { }
--- src/app/pages/models/activation.ts
+++ src/app/pages/models/activation.ts
@@ -4,12 +4,14 @@ export class Activation {
     public endIn: number;
     public numberOfParticipants: number;
     public imagePath: string;
+    public isActive: boolean;
 
-    constructor(name: string, created: number, endIn: number, numberOfParticipants: number, imagePath: string) {
+    constructor(name: string, created: number, endIn: number, numberOfParticipants: number, imagePath: string, isActive) {
         this.name = name;
         this.created = created;
         this.endIn = endIn;
         this.numberOfParticipants = numberOfParticipants;
         this.imagePath = imagePath;
+        this.isActive = isActive;
     }
 }
