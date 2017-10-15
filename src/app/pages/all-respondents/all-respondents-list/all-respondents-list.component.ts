836bdc63c8cbc566788179554ce4da2055ef6aab
Vitaliy Normatov <vnormatov@gmail.com>
2017-10-15 11:30:38 +0300

    fix bug

--- src/app/pages/all-respondents/all-respondents-list/all-respondents-list.component.ts
+++ src/app/pages/all-respondents/all-respondents-list/all-respondents-list.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {RespondentModel} from '../../models/respondent.model';
-import {UserModel} from '../../models/user.model';
-import {Test} from '../../models/test';
 import {GetRespondentsService} from '../../../shared/services/getRespondents.service';
 
 @Component({
@@ -11,7 +9,7 @@ import {GetRespondentsService} from '../../../shared/services/getRespondents.ser
 })
 export class AllRespondentsListComponent implements OnInit {
 
-  private respondentsList: RespondentModel[];
+  public respondentsList: RespondentModel[];
   promiseRespondents: Promise<RespondentModel[]>;
   errorMessage: String;
 
@@ -20,20 +18,6 @@ export class AllRespondentsListComponent implements OnInit {
   }
 
   ngOnInit() {
-    /*let userOne: UserModel;
-    let userTwo: UserModel;
-    let testOne: Test;
-    let testTwo: Test;
-    userOne = new UserModel('userNameFirst', 'userDescriptionFirst', 'lastVisited', 'lastTested');
-    userTwo = new UserModel('userNameSecond', 'userDescriptionSecond', 'lastVisited', 'lastTested');
-    testOne = new Test('Тест по HTML. Средний уровень', 5, 12);
-    testTwo = new Test('Тест по Git. Начальный уровень', 4, 10);
-
-     this.respondentsList = [
-       new RespondentModel(userOne, testOne, 1, 5, 'time'),
-       new RespondentModel(userTwo, testTwo, 2, 10, 'time')
-     ];*/
-
     this.promiseRespondents = this.request.get();
     this.promiseRespondents.then(
       respondent => this.respondentsList = respondent,
