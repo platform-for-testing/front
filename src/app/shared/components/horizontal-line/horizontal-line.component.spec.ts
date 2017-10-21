eb2dcb4392836f6ef284d5ae984f261e09367ad7
Юрий Рыбин <yrbn86@gmail.com>
2017-10-22 00:40:18 +0300

    fixed travis bugs

--- src/app/shared/components/horizontal-line/horizontal-line.component.spec.ts
+++ src/app/shared/components/horizontal-line/horizontal-line.component.spec.ts
@@ -1,14 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 
-<<<<<<< HEAD:src/app/pages/create-new-test/question-list/question/question.component.spec.ts
-import { QuestionComponent } from './question.component';
-import { Question } from './question';
-
-const questionMock = new Question(1, 'Question', '', true, true);
-=======
 import { HorizontalLineComponent } from './horizontal-line.component';
->>>>>>> 898643461270cbab691c2a97b771604e5b5e08de:src/app/shared/components/horizontal-line/horizontal-line.component.spec.ts
 
 describe('HorizontalLineComponent', () => {
   let component: HorizontalLineComponent;
@@ -16,12 +9,7 @@ describe('HorizontalLineComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-<<<<<<< HEAD:src/app/pages/create-new-test/question-list/question/question.component.spec.ts
-      declarations: [ QuestionComponent ],
-      imports: [FormsModule]
-=======
       declarations: [ HorizontalLineComponent ]
->>>>>>> 898643461270cbab691c2a97b771604e5b5e08de:src/app/shared/components/horizontal-line/horizontal-line.component.spec.ts
     })
     .compileComponents();
   }));
@@ -32,7 +20,6 @@ describe('HorizontalLineComponent', () => {
   });
 
   it('should create', () => {
-    component.question = questionMock;
     fixture.detectChanges();
     expect(component).toBeTruthy();
   });
