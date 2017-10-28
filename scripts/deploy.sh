56e50a5ca81bf664de4b4cf2c401cb475922b442
Yurii Holiuk <yuriiholiuk@gmail.com>
2017-10-28 21:00:50 +0300

    add bash file on server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-ssh $DEPLOY_USER@$DEPLOY_HOST
-cd /home/deploy/front-dev
-rm -rf build
-rm server.js
-tar -xzf package.tgz
-rm package.tgz
-node server.js
+ssh $DEPLOY_USER@$DEPLOY_HOST sh /home/deploy/front-dev/scripts/deploy.sh
