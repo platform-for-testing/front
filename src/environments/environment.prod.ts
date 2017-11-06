639f54c63184362136e6f824e8f2c169397f5cd8
Yurii Holiuk <yuriiholiuk@gmail.com>
2017-11-06 22:34:39 +0200

    small fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,12 @@ export const environment = {
   production: true,
   api: {
     quiz: {
-      save: 'http://95.85.54.211:3002/tests'
+      save: 'http://95.85.54.211:3002/tests',
+      get: 'http://95.85.54.211:3002/tests',
     },
     respondents: {
-      getAll: 'http://95.85.54.211:3002/respondents'
+      get: 'http://95.85.54.211:3002/respondents',
+      save: 'http://95.85.54.211:3002/respondents',
     }
   }
 };
