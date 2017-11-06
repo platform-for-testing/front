45a67513fe2a9333f9770c2246f141935e9803ab
Yurii Holiuk <yuriiholiuk@gmail.com>
2017-11-06 22:27:11 +0200

    build to prod on travis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,10 +7,10 @@ export const environment = {
   production: false,
   api: {
     quiz: {
-      save: 'http://95.85.54.211:3002/tests'
+      save: 'http://localhost:3000/tests'
     },
     respondents: {
-      getAll: 'http://95.85.54.211:3002/respondents'
+      getAll: 'http://localhost:3000/respondents'
     }
   }
 };
