8372f2258693345449557573bdfbaa61fd00831d
dzudzylo <skyerdzudzylo@gmail.com>
2017-11-11 11:45:53 +0200

    fix bugs

--- src/app/pages/models/question.ts
+++ src/app/pages/models/question.ts
@@ -6,11 +6,7 @@ export class Question {
   public image: string;
   public points: any;
 
-<<<<<<< HEAD:src/app/pages/create-new-test/question-list/question/question.ts
   constructor(title: string = 'Question', description: string = '', required: boolean = false, type: any = '', image: string = '', points: any = '') {
-=======
-  constructor(title: string = 'Question', description: string = '', required: boolean = false, type: number = 0, points: any = 1) {
->>>>>>> d018cb02958d97dcbe96e31d9072139be62d79fe:src/app/pages/models/question.ts
     this.title = title;
     this.description = description;
     this.required = required;
