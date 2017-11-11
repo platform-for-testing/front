36f1dc2159385ec1157b4cf7bd39c69aa7688d6d
Sergey Tatsiak <tatsiak@gmail.com>
2017-11-11 11:16:21 +0200

    fixed lint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import {HeaderComponent} from './shared/components/header/header.component';
-import {NavigationButtonsComponent} from './shared/components/header/navigation-buttons/navigation-buttons.component';
-import {ToolsComponent} from './shared/components/header/tools/tools.component';
-import {CreateNewComponent} from './shared/components/create-new/create-new.component';
-import {ModalComponent} from "./shared/components/modal/modal.component";
+import { HeaderComponent } from './shared/components/header/header.component';
+import { NavigationButtonsComponent } from './shared/components/header/navigation-buttons/navigation-buttons.component';
+import { ToolsComponent } from './shared/components/header/tools/tools.component';
+import { CreateNewComponent } from './shared/components/create-new/create-new.component';
+import { ModalComponent } from './shared/components/modal/modal.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
