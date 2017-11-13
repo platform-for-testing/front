e3048869a556f35f1bc8a68d226a7dc98af85300
yuchiko <iyuchiko@gmail.com>
2017-11-14 00:22:06 +0200

    change listLength type to number, wrap listLength into square brackets from string interpolation

--- src/app/pages/layout/list-page/list-page.component.ts
+++ src/app/pages/layout/list-page/list-page.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ListPageComponent implements OnInit {
   @Input() heading = 'default heading';
-  @Input() listLength = '0';
+  @Input() listLength = 0;
   constructor() { }
 
   ngOnInit() {
